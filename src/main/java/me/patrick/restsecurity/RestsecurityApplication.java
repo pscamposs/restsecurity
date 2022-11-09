f26b756b79d56b901b7e43273f1c14a2f7b2566f
Drakiz <ydrakiz@gmail.com>
2022-11-09 19:11:40 -0300

    First Commit

---
+++ src/main/java/me/patrick/restsecurity/RestsecurityApplication.java
@@ -0,0 +1,13 @@
+package me.patrick.restsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestsecurityApplication.class, args);
+    }
+
+}
